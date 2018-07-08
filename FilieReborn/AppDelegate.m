e6b8a9804343a4a1a94f8d9322d09312d34fd6b7
Uli Kusterer <ulivc@zathras.de>
2018-07-08 22:26:29 +0200

    Initial Commit

---
+++ FilieReborn/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  FilieReborn
+//
+//  Created by Uli Kusterer on 08.07.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
---
+++ FilieReborn/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FilieReborn
+//
+//  Created by Uli Kusterer on 08.07.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
