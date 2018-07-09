241e43ae03ab8f53e72b4e10074cb3e3f6d84b15
Uli Kusterer <ulivc@zathras.de>
2018-07-09 18:10:28 +0200

    Hide grow box in inactive windows.

---
+++ FilieReborn/FILActiveOnlyImageView.h
@@ -0,0 +1,13 @@
+//
+//  FILActiveOnlyImageView.h
+//  Filie
+//
+//  Created by Uli Kusterer on 09.07.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FILActiveOnlyImageView : NSImageView
+
+@end
---
+++ FilieReborn/FILActiveOnlyImageView.m
@@ -0,0 +1,44 @@
+//
+//  FILActiveOnlyImageView.m
+//  Filie
+//
+//  Created by Uli Kusterer on 09.07.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import "FILActiveOnlyImageView.h"
+
+@implementation FILActiveOnlyImageView
+
+-(void) viewWillMoveToWindow:(NSWindow *)newWindow
+{
+	if (self.window)
+	{
+		[NSNotificationCenter.defaultCenter removeObserver: self name: NSWindowDidBecomeKeyNotification object: self.window];
+		[NSNotificationCenter.defaultCenter removeObserver: self name: NSWindowDidResignKeyNotification object: self.window];
+		[NSNotificationCenter.defaultCenter removeObserver: self name: NSWindowDidBecomeMainNotification object: self.window];
+		[NSNotificationCenter.defaultCenter removeObserver: self name: NSWindowDidResignMainNotification object: self.window];
+	}
+}
+
+
+-(void)keyOrMainStatusDidChange: (NSNotification *)notification
+{
+	self.hidden = !self.window.isKeyWindow && !self.window.isMainWindow;
+}
+
+
+-(void) viewDidMoveToWindow
+{
+	if (self.window)
+	{
+		[NSNotificationCenter.defaultCenter addObserver: self selector: @selector(keyOrMainStatusDidChange:) name: NSWindowDidBecomeKeyNotification object: self.window];
+		[NSNotificationCenter.defaultCenter addObserver: self selector: @selector(keyOrMainStatusDidChange:) name: NSWindowDidResignKeyNotification object: self.window];
+		[NSNotificationCenter.defaultCenter addObserver: self selector: @selector(keyOrMainStatusDidChange:) name: NSWindowDidBecomeMainNotification object: self.window];
+		[NSNotificationCenter.defaultCenter addObserver: self selector: @selector(keyOrMainStatusDidChange:) name: NSWindowDidResignMainNotification object: self.window];
+
+		self.hidden = !self.window.isKeyWindow && !self.window.isMainWindow;
+	}
+}
+
+@end
